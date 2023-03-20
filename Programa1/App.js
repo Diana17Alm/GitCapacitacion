c35e1f24f1bf43e303ebee7fa2abc1c8cfc8935a
Diana <diana.almeida@clearminds-it.com>
2023-03-20 11:17:00 -0500

    Cambios pequeños

--- Programa1/App.js
+++ Programa1/App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { Image } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 
 export default function App() {
